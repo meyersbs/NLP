f3730f6b4b75cd2bd02ffbc54800eb393416db1c
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-09-08 13:46:09 -0400

    Create soundex_translator.py

---
+++ soundex_translator.py
@@ -0,0 +1,32 @@
+#!usr/bin/python
+
+import sys
+
+soundex_dict = {'B': '1', 'F': '1', 'P': '1', 'V': '1', 'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2', 'D': '3', 'T': '3', 'L': '4', 'M': '5', 'N': '5', 'R': '6', 'A': '',  'E': '',  'I': '',  'O': '', 'U': '',  'H': '',  'W': '',  'Y': ''}
+
+def translate_to_soundex(name_string):
+    global soundex_dict
+    if len(name_string) == 0:
+        return 'Found Empty String: No Translation Available.'
+    soundex_string = name_string[1:]
+         
+    for key in sorted(soundex_dict.keys()):
+        soundex_string = soundex_string.replace(key, soundex_dict[key])   
+
+    length = len(soundex_string)    
+    if length < 3:
+        soundex_string += '0'*(3-length)
+        
+    return name_string[0] + soundex_string[:3]
+
+def translate_from_soundex(soundex_string):
+    return 'Feature Not Yet Implemented.'
+                
+def main():
+    if len(sys.argv) == 2:
+        print(translate_to_soundex(sys.argv[1].upper()))
+    else:
+        print 'You Must Provide A String To Be Translated.'
+
+if __name__ == "__main__":
+    main()
